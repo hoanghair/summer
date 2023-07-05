0e7c7680bd6de90158eb668d3ccdec2efaa48fa0
hoanghair <96115510+hoanghair@users.noreply.github.com>
2023-07-05 07:06:13 +0700

    update code

---
+++ script.js
@@ -0,0 +1,4 @@
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   const animatedText = document.getElementsByClassName("txt-animation");
+//   animatedText.style.animationPlayState = "running";
+// });
