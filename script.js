cbb31fe7a5034925271d753b495f012305d4c0a0
hainh <hoanghai16072000@gmail.com>
2023-07-06 17:24:36 +0700

    update code company

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function (e) {
   // active
-  $(".nav__item").click(function () {
+  $(".nav__link").click(function () {
     $(".nav__item").removeClass("is_active");
-    $(this).addClass("is_active");
+    $(this).parents('.nav__item').addClass("is_active");
   });
 
   $(".nav__link").on("click", function (e) {
@@ -14,12 +14,27 @@ $(document).ready(function (e) {
     e.preventDefault();
     var targetLocation = $(this).attr("href");
     var topOffSet = Math.round($(targetLocation).offset().top);
-    $("HTML, body").animate({ scrollTop: topOffSet }, 1000);
+    $("html, body").animate({ scrollTop: topOffSet }, 400);
   });
 
   $(".scrolldown").click(function () {
     var targetLocation = $(".nav__link").attr("href");
     var topOffset = Math.round($(targetLocation).offset().top);
-    $("html, body").animate({ scrollTop: topOffset }, 1000);
+    $("html, body").animate({ scrollTop: topOffset }, 400);
   });
+
+  // $(window).scroll(function (e) {
+  //   e.preventDefault();
+  //   var scrollActiveClass = $('html, body').scrollTop()
+  //   var offSetTopRegister = Math.round($('.register').offset().top)
+  //   console.log(scrollActiveClass);
+  //   if(scrollActiveClass >= offSetTopRegister){
+  //     $(".nav__item").removeClass("is_active");
+  //     $('.nav__item:first-child').addClass("is_active");
+  //   }else {
+  //     $('.nav__item').removeClass("is_active");
+  //   }
+    
+  // })
+
 });
